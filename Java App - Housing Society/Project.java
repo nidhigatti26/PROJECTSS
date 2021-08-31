cc61898b625c554c3c625c505a7c2766da82b4bc
nidhigatti26 <77826836+nidhigatti26@users.noreply.github.com>
2021-08-31 15:59:40 +0530

    Add files via upload

---
+++ Java App - Housing Society/Project.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+
+
+/**
+ *
+ * @author Shreyas
+ */
+public class Project {
+
+   
+    public static void main(String[] args) {
+        new login().setVisible(true);
+        //new NewJFrame().setVisible(true);
+       // new Registration_form().setVisible(true);
+	      
+    }
+    
+}
